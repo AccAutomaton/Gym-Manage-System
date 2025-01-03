1e2a0ebc3401b8f70d368b6c4de1642d4941b751
AcAutomaton <88975453+AccAutomaton@users.noreply.github.com>
2025-01-03 16:04:04 +0800

    refactor: EquipmentDao.java

--- src/main/java/com/acautomaton/gym/dao/EquipmentDao.java
+++ src/main/java/com/acautomaton/gym/dao/EquipmentDao.java
@@ -1,13 +1,7 @@
 package com.acautomaton.gym.dao;
 
-
 import com.acautomaton.gym.entity.Equipment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-/**
- * @Description: 设备信息Dao层接口
- * @Author: LiuJian
- * @Date: 2020/4/3
- */
-public interface EquipmentDao extends JpaRepository<Equipment,Long> {
+public interface EquipmentDao extends JpaRepository<Equipment, Long> {
 }
