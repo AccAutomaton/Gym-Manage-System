3f6f5dc377e9b9a0096423f866fe61b4577b16f3
AcAutomaton <88975453+AccAutomaton@users.noreply.github.com>
2025-01-03 15:34:39 +0800

    refactor: PrivateCoachInfoController.java

--- src/main/java/com/acautomaton/gym/controller/PrivateCoachInfoController.java
+++ src/main/java/com/acautomaton/gym/controller/PrivateCoachInfoController.java
@@ -4,9 +4,8 @@ import com.acautomaton.gym.dao.CoachDao;
 import com.acautomaton.gym.dao.MenberDao;
 import com.acautomaton.gym.dao.PrivateCoachInfoDao;
 import com.acautomaton.gym.dao.SubjectDao;
-import com.acautomaton.gym.dao.*;
-import com.acautomaton.gym.service.PrivateCoachInfoDaoImpl;
 import com.acautomaton.gym.entity.PrivateCoachInfo;
+import com.acautomaton.gym.service.PrivateCoachInfoDaoImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -16,155 +15,99 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.*;
 
-/**
- * @Description: 会员私教详情Controller控制层
- * @Author: LiuJian
- * @Date: 2020/4/9
- */
 @Controller
 @RequestMapping("/privateinfo")
 public class PrivateCoachInfoController {
+    private final CoachDao coachDao;
+    private final SubjectDao subjectDao;
+    private final MenberDao menberDao;
+    private final PrivateCoachInfoDao privateCoachInfoDao;
+    private final PrivateCoachInfoDaoImpl privateCoachInfoDaoImpl;
 
     @Autowired
-    private CoachDao coachDao;
-    @Autowired
-    private SubjectDao subjectDao;
-    @Autowired
-    private MenberDao menberDao;
+    public PrivateCoachInfoController(CoachDao coachDao, SubjectDao subjectDao, MenberDao menberDao,
+                                      PrivateCoachInfoDao privateCoachInfoDao,
+                                      PrivateCoachInfoDaoImpl privateCoachInfoDaoImpl) {
+        this.coachDao = coachDao;
+        this.subjectDao = subjectDao;
+        this.menberDao = menberDao;
+        this.privateCoachInfoDao = privateCoachInfoDao;
+        this.privateCoachInfoDaoImpl = privateCoachInfoDaoImpl;
+    }
 
-    @Autowired
-    private PrivateCoachInfoDao privateCoachInfoDao;
-    @Autowired
-    private PrivateCoachInfoDaoImpl privateCoachInfoDaoImpl;
-
-    /**
-     * @Description: 会员私教课程-进入会员私教课程界面
-     * @Author: LiuJian
-     * @Date: 2020/4/9
-     */
     @RequestMapping("/jin3")
-    public String jin3(){
-
+    public String jin3() {
         return "WEB-INF/jsp/privatecoach";
     }
 
-    /**
-     * @Description: 会员私教详情-查询所有会员私教信息
-     * @Author: LiuJian
-     * @Date: 2020/4/9
-     */
     @RequestMapping("/ddaa")
     @ResponseBody
-    public List<PrivateCoachInfo> query(){
-       List<PrivateCoachInfo> list=privateCoachInfoDao.findAll();
-//       for (PrivateCoachInfo li:list){
-//           System.out.println(li.getMember().getMemberName());
-//       }
-     return   privateCoachInfoDao.findAll();
+    public List<PrivateCoachInfo> query() {
+        return privateCoachInfoDao.findAll();
     }
 
-    /**
-     * @Description: 会员私教详情-查询教练,课程,会员信息
-     * @Author: LiuJian
-     * @Date: 2020/4/9
-     */
     @ModelAttribute("a")
-    public Model cha(Model model){
-        model.addAttribute("coach",coachDao.findAll());
-        model.addAttribute("subject",subjectDao.findAll());
-        model.addAttribute("menber",menberDao.findAll());
-        return  model;
+    public Model cha(Model model) {
+        model.addAttribute("coach", coachDao.findAll());
+        model.addAttribute("subject", subjectDao.findAll());
+        model.addAttribute("menber", menberDao.findAll());
+        return model;
     }
 
-    /**
-     * @Description: 会员私教详情-根据教练id,课程id,会员id分页查询
-     * @Author: LiuJian
-     * @Date: 2020/4/9
-     */
     @RequestMapping("/query")
     @ResponseBody
-    public Map<String,Object> query2(Model model,Integer coachid, Integer subjectid, Integer memberid, int pageSize, int pageNumber){
-        Map<String,Object>  map1=new HashMap<String,Object>();
-        map1.put("coachid",coachid);
-        map1.put("subjectid",subjectid);
-        map1.put("memberid",memberid);
-        map1.put("qi",(pageNumber-1)*pageSize);
-        map1.put("shi",pageSize);
+    public Map<String, Object> query2(Model model, Integer coachid, Integer subjectid, Integer memberid, int pageSize, int pageNumber) {
+        Map<String, Object> map1 = new HashMap<>();
+        return getPrivateCoachMap(coachid, subjectid, memberid, pageSize, pageNumber, map1);
+    }
+
+    private Map<String, Object> getPrivateCoachMap(Integer coachid, Integer subjectid, Integer memberid, int pageSize, int pageNumber, Map<String, Object> map1) {
+        map1.put("coachid", coachid);
+        map1.put("subjectid", subjectid);
+        map1.put("memberid", memberid);
+        map1.put("qi", (pageNumber - 1) * pageSize);
+        map1.put("shi", pageSize);
         return privateCoachInfoDaoImpl.query(map1);
     }
 
-    /**
-     * @Description: 会员私教详情-根据id查询所有会员私教信息
-     * @Author: LiuJian
-     * @Date: 2020/4/9
-     */
     @RequestMapping("/query2")
     @ResponseBody
-    public List<PrivateCoachInfo> query3(long pid){
-        List<PrivateCoachInfo> list  = new ArrayList<PrivateCoachInfo>();
+    public List<PrivateCoachInfo> query3(long pid) {
+        List<PrivateCoachInfo> list = new ArrayList<>();
         list.add(privateCoachInfoDao.findById(pid).get());
         return list;
     }
 
-    /**
-     * @Description: 会员私教详情-根据id查询会员私教信息
-     * @Author: LiuJian
-     * @Date: 2020/4/9
-     */
     @RequestMapping("/cha")
     @ResponseBody
-    public Optional<PrivateCoachInfo> one(long id){
+    public Optional<PrivateCoachInfo> one(long id) {
         return privateCoachInfoDao.findById(id);
     }
 
-    /**
-     * @Description: 会员私教详情-删除会员私教信息
-     * @Author: LiuJian
-     * @Date: 2020/4/9
-     */
     @RequestMapping("/del")
     @ResponseBody
-    public  Map<String,Object> del(long id,Integer coachid,Integer subjectid, Integer memberid, int pageSize, int pageNumber){
+    public Map<String, Object> del(long id, Integer coachid, Integer subjectid, Integer memberid, int pageSize, int pageNumber) {
         privateCoachInfoDao.deleteById(id);
-        Map<String,Object>  map1=new HashMap<String,Object>();
-        map1.put("coachid",coachid);
-        map1.put("subjectid",subjectid);
-        map1.put("memberid",memberid);
-        map1.put("qi",(pageNumber-1)*pageSize);
-        map1.put("shi",pageSize);
-        return privateCoachInfoDaoImpl.query(map1);
+        Map<String, Object> map1 = new HashMap<>();
+        return getPrivateCoachMap(coachid, subjectid, memberid, pageSize, pageNumber, map1);
     }
 
-    /**
-     * @Description: 会员私教详情-修改会员私教信息
-     * @Author: LiuJian
-     * @Date: 2020/4/9
-     */
     @RequestMapping("/upd")
     @ResponseBody
-    public  void upd(PrivateCoachInfo privateCoachInfo){
+    public void upd(PrivateCoachInfo privateCoachInfo) {
         privateCoachInfoDaoImpl.update(privateCoachInfo);
     }
 
-    /**
-     * @Description: 会员私教详情-根据会员id查询所有会员私教信息的数量
-     * @Author: LiuJian
-     * @Date: 2020/4/9
-     */
     @RequestMapping("/count")
     @ResponseBody
-    public Long count (Integer memid){
+    public Long count(Integer memid) {
         privateCoachInfoDaoImpl.count(memid);
-        return  privateCoachInfoDaoImpl.count(memid);
+        return privateCoachInfoDaoImpl.count(memid);
     }
-    /**
-     * @Description: 会员私教详情-根据会员id在会员私教信息表中查询信息
-     * @Author: LiuJian
-     * @Date: 2020/4/9
-     */
+
     @RequestMapping("/byid")
     @ResponseBody
-    public List<PrivateCoachInfo> queryid(Long memberid){
+    public List<PrivateCoachInfo> queryid(Long memberid) {
         return privateCoachInfoDaoImpl.ByMemberid(memberid);
     }
 }
