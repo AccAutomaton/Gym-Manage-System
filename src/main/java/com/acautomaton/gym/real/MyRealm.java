f5bd8a886f7f4b90f1f54384c0d01d29ed83d5d2
AcAutomaton <88975453+AccAutomaton@users.noreply.github.com>
2025-01-04 14:50:06 +0800

    refactor: MyRealm.java

--- src/main/java/com/acautomaton/gym/real/MyRealm.java
+++ src/main/java/com/acautomaton/gym/real/MyRealm.java
@@ -2,13 +2,10 @@ package com.acautomaton.gym.real;
 
 import com.acautomaton.gym.dao.AdminUserDao;
 import com.acautomaton.gym.entity.AdminUser;
-import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.*;
 import org.apache.shiro.authz.AuthorizationInfo;
-import org.apache.shiro.authz.SimpleAuthorizationInfo;
 import org.apache.shiro.realm.AuthorizingRealm;
 import org.apache.shiro.subject.PrincipalCollection;
-import org.apache.shiro.subject.Subject;
 import org.springframework.beans.factory.annotation.Autowired;
 
 public class MyRealm extends AuthorizingRealm {
@@ -18,25 +15,17 @@ public class MyRealm extends AuthorizingRealm {
 
     @Override
     protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principalCollection) {
-        System.out.println("已授权");
-        Subject subject = SecurityUtils.getSubject();
-        AdminUser user = (AdminUser) principalCollection.getPrimaryPrincipal() ;
-        //
-        SimpleAuthorizationInfo info = new SimpleAuthorizationInfo() ;
         return null;
     }
 
     @Override
     protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
-
-        UsernamePasswordToken token = (UsernamePasswordToken)authenticationToken ;
-        AdminUser adminuser = adminuserDao.findByAdminNameAndAdminPassword(token.getUsername(),String.copyValueOf(token.getPassword()));
-
-        if(null == adminuser){
-            return null ;
+        UsernamePasswordToken token = (UsernamePasswordToken) authenticationToken;
+        AdminUser adminuser = adminuserDao.findByAdminNameAndAdminPassword(token.getUsername(), String.copyValueOf(token.getPassword()));
+        if (null == adminuser) {
+            return null;
         }
-
-        SimpleAuthenticationInfo info = new SimpleAuthenticationInfo(adminuser.getAdminName(),adminuser.getAdminPassword(),"") ;
+        SimpleAuthenticationInfo info = new SimpleAuthenticationInfo(adminuser.getAdminName(), adminuser.getAdminPassword(), "");
         System.out.println("完成认证！");
         return info;
     }
