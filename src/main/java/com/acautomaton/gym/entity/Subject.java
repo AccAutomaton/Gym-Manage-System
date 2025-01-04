06b468905547c2681a1be50489883b08d3f086fa
AcAutomaton <88975453+AccAutomaton@users.noreply.github.com>
2025-01-04 14:48:29 +0800

    style: Subject.java

--- src/main/java/com/acautomaton/gym/entity/Subject.java
+++ src/main/java/com/acautomaton/gym/entity/Subject.java
@@ -5,31 +5,14 @@ import lombok.Setter;
 
 import javax.persistence.*;
 
-/**
- * @Description: 课程信息实体类
- * @Author: LiuJian
- * @Date: 2020/4/3
- */
 @Entity
 @Table(name = "subject")
 @Getter
 @Setter
 public class Subject {
-  @Id
-  @GeneratedValue(strategy =  GenerationType.IDENTITY)
-  private long subId;
-  private String subname;
-  private double sellingPrice;
- /* @JoinColumn
-  private List<PrivateCoachInfo> privateCoachInfos;*/
-// @OneToMany
-// @JoinColumn(name = "subjectid")
-//  private PrivateCoachInfo privateCoachInfo;
-
-  /*
-
-  @OneToMany
-  @JsonIgnore
-  private List<PrivateCoach> privateCoachList;*/
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long subId;
+    private String subname;
+    private double sellingPrice;
 }
