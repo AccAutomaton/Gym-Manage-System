81ea2422c4020a8c6bce20d94c71172e4bc56689
AcAutomaton <88975453+AccAutomaton@users.noreply.github.com>
2025-01-04 14:41:59 +0800

    style: Equipment.java

--- src/main/java/com/acautomaton/gym/entity/Equipment.java
+++ src/main/java/com/acautomaton/gym/entity/Equipment.java
@@ -5,22 +5,14 @@ import lombok.Setter;
 
 import javax.persistence.*;
 
-/**
- * @Description: 设备信息实体类
- * @Author: LiuJian
- * @Date: 2020/4/3
- */
 @Entity
 @Table(name = "equipment")
 @Getter
 @Setter
 public class Equipment {
-  @Id
-  @GeneratedValue(strategy =  GenerationType.IDENTITY)
-  private long eqId;
-  private String eqName;
-  private String eqText;
-
-
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long eqId;
+    private String eqName;
+    private String eqText;
 }
