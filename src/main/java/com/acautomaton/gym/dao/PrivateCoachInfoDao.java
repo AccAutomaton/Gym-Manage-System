049d0b4634cdf640b36fee1b6c2b3d304ed2a80f
AcAutomaton <88975453+AccAutomaton@users.noreply.github.com>
2025-01-04 14:37:12 +0800

    style: PrivateCoachInfoDao.java

--- src/main/java/com/acautomaton/gym/dao/PrivateCoachInfoDao.java
+++ src/main/java/com/acautomaton/gym/dao/PrivateCoachInfoDao.java
@@ -1,6 +1,5 @@
 package com.acautomaton.gym.dao;
 
-
 import com.acautomaton.gym.entity.PrivateCoachInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -8,24 +7,13 @@ import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
-/**
- * @Description: 私教信息Dao层接口
- * @Author: LiuJian
- * @Date: 2020/4/3
- */
-public interface PrivateCoachInfoDao extends JpaRepository<PrivateCoachInfo,Long> {
-/*
-    @Query(value = "select p from PrivateCoachInfo p where p.memberid=:memberid")
-    public PrivateCoachInfo queryById(@Param("memberid") long memberid) ;*/
+public interface PrivateCoachInfoDao extends JpaRepository<PrivateCoachInfo, Long> {
+    @Query(value = "select * FROM  privatecoachinfo where memberid =:memberid", nativeQuery = true)
+    List<PrivateCoachInfo> queryByIdNative(@Param("memberid") long memberid);
 
+    @Query(value = "select * FROM  privatecoachinfo where coachid =:coachid", nativeQuery = true)
+    List<PrivateCoachInfo> queryByCoachIdNative(@Param("coachid") long coachid);
 
-    @Query(value = "select * FROM  privatecoachinfo where memberid =:memberid",nativeQuery = true)
-    public List<PrivateCoachInfo> queryByIdNative(@Param("memberid") long memberid);
-
-    @Query(value = "select * FROM  privatecoachinfo where coachid =:coachid",nativeQuery = true)
-    public List<PrivateCoachInfo> queryByCoachIdNative(@Param("coachid") long coachid);
-
-    @Query(value = "select * FROM  privatecoachinfo where subjectid =:subjectid",nativeQuery = true)
-    public List<PrivateCoachInfo> queryBySubjectIdNative(@Param("subjectid") long subjectid);
-
+    @Query(value = "select * FROM  privatecoachinfo where subjectid =:subjectid", nativeQuery = true)
+    List<PrivateCoachInfo> queryBySubjectIdNative(@Param("subjectid") long subjectid);
 }
