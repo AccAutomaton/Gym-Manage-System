5a232b8b6c512a46808977ba26519836be95f42e
AcAutomaton <88975453+AccAutomaton@users.noreply.github.com>
2025-01-04 14:47:42 +0800

    style: Recharge.java

--- src/main/java/com/acautomaton/gym/entity/Recharge.java
+++ src/main/java/com/acautomaton/gym/entity/Recharge.java
@@ -6,35 +6,26 @@ import lombok.Setter;
 
 import javax.persistence.*;
 
-/**
- * @Description: 充值信息实体类
- * @Author: LiuJian
- * @Date: 2020/4/3
- */
 @Getter
 @Setter
 @Table(name = "chongzhi")
 @Entity
 public class Recharge {
-  @Id
-  @GeneratedValue(strategy =  GenerationType.IDENTITY)
-  private long id;
-
-  @OneToOne
-  @JoinColumn(name = "Memberid")
-  private Member member;
-
-  @OneToOne
-  @JoinColumn(name = "Typeid")
-  private MemberType membertype;
-  private long money;
-  private long ssmoney;
-  private long zlmoney;
-  @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone="GMT+8")
-  private java.sql.Timestamp date;
-  private long czjine;
-  private String beizhu;
-  private long czStatic;
-
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @OneToOne
+    @JoinColumn(name = "Memberid")
+    private Member member;
+    @OneToOne
+    @JoinColumn(name = "Typeid")
+    private MemberType membertype;
+    private long money;
+    private long ssmoney;
+    private long zlmoney;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    private java.sql.Timestamp date;
+    private long czjine;
+    private String beizhu;
+    private long czStatic;
 }
